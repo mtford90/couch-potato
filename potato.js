29f5f5fa3b808d30ce70d841c68fbc67308da959
Michael Ford <mtford@gmail.com>
2015-02-07 11:03:04 +0000

    no more extendAPI

--- potato.js
+++ potato.js
@@ -15,29 +15,6 @@
         EventEmitter = require('events').EventEmitter;
 
     /**
-     * This is used in construction of the API object.
-     * It pulls public functions from each module and places them on the API object.
-     * @param api - The object we're extending
-     * @param mod - The module from which we will pull functions.
-     */
-    function extendAPI(api, mod) {
-        for (var funcName in mod) {
-            var isPublicFunction = funcName[0] != '_';
-            if (isPublicFunction) {
-                var func = mod[funcName];
-                if (func instanceof Function) {
-                    if (!api[funcName]) {
-                        api[funcName] = func.bind(mod);
-                    }
-                    else {
-                        throw new Error(funcName + ' already exists on the API object.');
-                    }
-                }
-            }
-        }
-    }
-
-    /**
      * @param opts
      * @param opts.database
      * @param opts.http
@@ -93,8 +70,6 @@
         this.auth = auth;
         this.http = ajax;
         this.accounts = this.users = this.account = users;
-
-        extendAPI(this, auth);
 
         for (var prop in constants) {
             if (constants.hasOwnProperty(prop)) this[prop] = constants[prop];
